866e888762beee401ca1586813f2577fc432b4d9
fischer.liu <fischer.liu@linecorp.com>
2019-03-17 23:04:37 +0800

    Done with server-sdie cache

---
+++ src/server/cacheProvider.js
@@ -0,0 +1,27 @@
+class CacheProvider {
+  constructor({ maxAge, shouldForceUpdate }) {
+    this._cache = {};
+    this._maxAge = maxAge;
+    this._shouldForceUpdate = shouldForceUpdate;
+  }
+
+  get(req) {
+    const key = req.path;
+    const cache = this._cache[key];
+    let hasCache = !!cache
+    if (hasCache) hasCache = Date.now() - cache.updateTime <= this._maxAge
+    if (hasCache) hasCache = this._shouldForceUpdate ? !this._shouldForceUpdate(req) : hasCache;
+    if (hasCache) return cache.data;
+    return null;
+  }
+
+  set(req, data) {
+    const key = req.path;
+    this._cache[key] = {
+      data,
+      updateTime: Date.now(),
+    };
+  }
+};
+
+module.exports = CacheProvider;
--- src/server/server.js
+++ src/server/server.js
@@ -1,20 +1,33 @@
-const express = require('express');
 const http = require('http');
+const axios = require('axios');
+const express = require('express');
+const compression = require('compression');
+
 const utils = require('../build/utils');
 const { env, port, publicDir } = require('../build/config');
-const axios = require('axios');
+const CacheProvider = require('./CacheProvider');
 const stockProvider = require('./stockProvider')({ env, axios });
 
 const PUBLIC_DIR = publicDir;
 const PORT = process.env.PORT || port;
-
+const cache = new CacheProvider({
+  maxAge: 10 * 60 * 1000, // 10 mins
+  shouldForceUpdate(req) {
+    const noCache = req.param("noCache");
+    return noCache ? noCache.toLowerCase() === 'true' : false;
+  },
+})
 const app = express();
 
-// Server static files
+app.use(compression());
 app.use(express.static(PUBLIC_DIR));
 
 app.get("/stockdata/:id", async (req, res) => {
-  const data = await stockProvider.get(req.params.id)
+  let data = cache.get(req)
+  if (!data) {
+    data = await stockProvider.get(req.params.id);
+    cache.set(req, data);
+  }
   res.json(data);
 });
 
