16a5b498b4c3b559f8caa23aa7c0ef2e6ca9a031
fischer.liu <fischer.liu@linecorp.com>
2019-05-01 15:49:39 +0800

    Refactor Table.jsx

--- src/client/components/Table.jsx
+++ src/client/components/Table.jsx
@@ -23,6 +23,23 @@ function renderHeader(headerSubText = {}) {
   );
 }
 
+function renderRow({ title, cells }) {
+  const tableCells = cells.map(([ content, subText ], i) => {
+    const key = i + content + (subText || '');
+    return <Table.Cell key={key}>{ content || '' }<p className={subTextClass(subText)}>{ subText }</p></Table.Cell>;
+  });
+  return (
+    <Table.Row>
+      <Table.Cell>{ title[0] }<p className={subTextClass(title[1])}>{ title[1] }</p></Table.Cell>
+      { tableCells }
+    </Table.Row>
+  );
+}
+renderRow.propTypes = {
+  title: PropTypes.arrayOf(PropTypes.string).isRequired,
+  cells: PropTypes.array.isRequired,
+};
+
 class TableByYears extends Component {
   render() {
     const [ [top5Price, top5Eps], [mid5Price, mid5Eps], [low5Price, low5Eps] ] = this.props.prices5yrs;
@@ -30,24 +47,27 @@ class TableByYears extends Component {
     return (
       <Table unstackable selectable color={this.props.color} className="table">
         { renderHeader() }
-
         <Table.Body>
-          <Table.Row>
-            <Table.Cell>5 Years</Table.Cell>
-            <Table.Cell>{ low5Price }<p className="table-subText">({ low5Eps })</p></Table.Cell>
-            <Table.Cell className="table-subText">{ round((low5Price + mid5Price) / 2) }</Table.Cell>
-            <Table.Cell>{ mid5Price }<p className="table-subText">({ mid5Eps })</p></Table.Cell>
-            <Table.Cell className="table-subText">{ round((mid5Price + top5Price) / 2) }</Table.Cell>
-            <Table.Cell>{ top5Price }<p className="table-subText">({ top5Eps })</p></Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.Cell>3 Years</Table.Cell>
-            <Table.Cell>{ low3Price }<p className="table-subText">({ low3Eps })</p></Table.Cell>
-            <Table.Cell className="table-subText">{ round((low3Price + mid3Price) / 2) }</Table.Cell>
-            <Table.Cell>{ mid3Price }<p className="table-subText">({ mid3Eps })</p></Table.Cell>
-            <Table.Cell className="table-subText">{ round((top3Price + mid3Price) / 2) }</Table.Cell>
-            <Table.Cell>{ top3Price }<p className="table-subText">({ top3Eps })</p></Table.Cell>
-          </Table.Row>
+          { renderRow({
+            title: [ '5 Years' ],
+            cells: [
+              [ low5Price, `(${low5Eps})` ],
+              [ '', round((low5Price + mid5Price) / 2) ],
+              [ mid5Price, `(${mid5Eps})` ],
+              [ '', round((mid5Price + top5Price) / 2) ],
+              [ top5Price, `(${top5Eps})` ],
+            ],
+          }) }
+          { renderRow({
+            title: [ '3 Years' ],
+            cells: [
+              [ low3Price, `(${low3Eps})` ],
+              [ '', round((low3Price + mid3Price) / 2) ],
+              [ mid3Price, `(${mid3Eps})` ],
+              [ '', round((mid3Price + top3Price) / 2) ],
+              [ top3Price, `(${top3Eps})` ],
+            ],
+          }) }
         </Table.Body>
       </Table>
     );
@@ -62,7 +82,6 @@ TableByYears.propTypes = {
   prices3yrs: PropTypes.arrayOf(arrayOfPriceEps).isRequired,
 };
 
-
 class TableByDividends extends Component {
   render() {
     const [ currDividend, topCurrPrice, midCurrPrice, lowCurrPrice ] = this.props.priceByCurrDividend;
@@ -70,24 +89,27 @@ class TableByDividends extends Component {
     return (
       <Table unstackable selectable color={this.props.color} className="table">
         { renderHeader({ low: '6.25%', mid: '4%', top: '2.5%' }) }
-
         <Table.Body>
-          <Table.Row>
-            <Table.Cell>Current Dividends<p className="table-subText">({ currDividend })</p></Table.Cell>
-            <Table.Cell>{ lowCurrPrice }</Table.Cell>
-            <Table.Cell className="table-subText">{ round((lowCurrPrice + midCurrPrice) / 2) }</Table.Cell>
-            <Table.Cell>{ midCurrPrice }</Table.Cell>
-            <Table.Cell className="table-subText">{ round((midCurrPrice + topCurrPrice) / 2) }</Table.Cell>
-            <Table.Cell>{ topCurrPrice }</Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.Cell>Average Dividends<p className="table-subText">({ AvgDividend })</p></Table.Cell>
-            <Table.Cell>{ lowAvgPrice }</Table.Cell>
-            <Table.Cell className="table-subText">{ round((lowAvgPrice + midAvgPrice) / 2) }</Table.Cell>
-            <Table.Cell>{ midAvgPrice }</Table.Cell>
-            <Table.Cell className="table-subText">{ round((midAvgPrice + topAvgPrice) / 2) }</Table.Cell>
-            <Table.Cell>{ topAvgPrice }</Table.Cell>
-          </Table.Row>
+          { renderRow({
+            title: [ 'Current Dividends', `(${currDividend})` ],
+            cells: [
+              [ lowCurrPrice ],
+              [ '', round((lowCurrPrice + midCurrPrice) / 2) ],
+              [ midCurrPrice ],
+              [ '', round((midCurrPrice + topCurrPrice) / 2) ],
+              [ topCurrPrice ],
+            ],
+          }) }
+          { renderRow({
+            title: [ 'Average Dividends', `(${AvgDividend})` ],
+            cells: [
+              [ lowAvgPrice ],
+              [ '', round((lowAvgPrice + midAvgPrice) / 2) ],
+              [ midAvgPrice ],
+              [ '', round((midAvgPrice + topAvgPrice) / 2) ],
+              [ topAvgPrice ],
+            ],
+          }) }
         </Table.Body>
       </Table>
     );
