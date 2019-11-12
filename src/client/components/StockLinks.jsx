fe5df57c1007e57e9ac061aa30eb4cfa62df0a23
Fischer-L <Fischer-L@users.noreply.github.com>
2019-11-12 19:48:47 +0800

    Enable to open all stock links at once (#42)

--- src/client/components/StockLinks.jsx
+++ src/client/components/StockLinks.jsx
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { List } from 'semantic-ui-react';
+import { List, Button } from 'semantic-ui-react';
+
+import '@/css/StockLinks.scss';
 
 class StockLinks extends Component {
-  render() {
-    const { stock, className } = this.props;
+  constructor(props) {
+    super(props);
+
+    this.onClick = (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+      const urls = this.createURLs(this.props);
+      for (let i = urls.length - 1; i >= 0; --i) {
+        window.open(urls[i], '_blank');
+      }
+      return false;
+    };
+  }
+
+  createURLs({ stock }) {
     const nameEncoded = encodeURIComponent(stock.name);
-    const urls = [
+    return [
       `https://www.wantgoo.com/stock/astock/techchart?stockno=${stock.id}`,
       `https://goodinfo.tw/StockInfo/ShowBuySaleChart.asp?CHT_CAT=DATE&STOCK_ID=${stock.id}`,
       `https://www.google.com/search?tbm=nws&q=${stock.id}+${nameEncoded}`,
@@ -14,14 +29,20 @@ class StockLinks extends Component {
       `https://www.cmoney.tw/follow/channel/stock-${stock.id}`,
       `https://www.google.com/search?tbs=qdr:w&q=${nameEncoded}+site:www.ptt.cc/bbs/Stock`,
     ];
+  }
+
+  render() {
+    const urls = this.createURLs(this.props);
+    const classes = [ 'stockLinks', this.props.className || '' ];
     return (
-      <List className={className || ''} bulleted horizontal size="large">
-        <List.Item as="a" target="_blank" href={urls[0]} onClick={this.TMP_onClick}>技術</List.Item>
+      <List className={classes.join(' ')} bulleted horizontal size="large">
+        <List.Item as="a" target="_blank" href={urls[0]}>技術</List.Item>
         <List.Item as="a" target="_blank" href={urls[1]}>籌碼</List.Item>
         <List.Item as="a" target="_blank" href={urls[2]}>News</List.Item>
         <List.Item as="a" target="_blank" href={urls[3]}>24hrs News</List.Item>
         <List.Item as="a" target="_blank" href={urls[4]}>討論</List.Item>
         <List.Item as="a" target="_blank" href={urls[5]}>Ptt</List.Item>
+        <Button className="stockLinks-openBtn" icon="globe" circular onClick={this.onClick} />
       </List>
     );
   }
