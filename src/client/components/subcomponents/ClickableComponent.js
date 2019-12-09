c579457d3f6b58f34fc1a55c539804cd7259fac0
fischer.liu <fischer.liu@linecorp.com>
2019-12-10 00:15:49 +0800

    Create ClickableComponent

---
+++ src/client/components/subcomponents/ClickableComponent.js
@@ -0,0 +1,48 @@
+// import { Component } from 'react'; TODO: Will need after removing EventDispatcher
+import EventDispatcher from '@/components/subcomponents/EventDispatcher';
+
+class ClickableComponent extends EventDispatcher {
+  constructor(props) {
+    super(props);
+
+    this._handlers = [];
+
+    this.onClick = (e) => {
+      if (this._handlers.length === 0) return;
+
+      if (e.type === 'touchend') this._isTouchHandled = true;
+
+      if (e.type === 'click' && this._isTouchHandled) {
+        e.preventDefault();
+        e.stopPropagation();
+        return;
+      }
+
+      e.persist();
+      let { target } = e;
+      while (target) {
+        for (const handler of this._handlers) { // eslint-disable-line no-restricted-syntax
+          if (handler(e, target)) {
+            e.preventDefault();
+            e.stopPropagation();
+            return;
+          }
+        }
+        if (this._stopTraversingDOM(e, target)) {
+          target = null;
+        }
+        target = target && target.parentElement;
+      }
+    };
+  }
+
+  _stopTraversingDOM(e, target) { // eslint-disable-line no-unused-vars
+    return false;
+  }
+
+  regisHandler(...handlers) {
+    this._handlers.push(...handlers.filter(h => typeof h === 'function'));
+  }
+}
+
+export default ClickableComponent;
