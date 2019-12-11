d96b515bd2d709a5c06fec16e40f62f2d560f054
fischer.liu <fischer.liu@linecorp.com>
2019-12-11 16:14:04 +0800

    Test move-docker-run-test-on-deploy-2

--- src/build/heroku_release.sh
+++ src/build/heroku_release.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
+echo TMP STOP...
+# echo "$HEROKU_API_KEY" | docker login -u "$HEROKU_USERNAME" --password-stdin "$HEROKU_REGISTRY"
 
-echo "$HEROKU_API_KEY" | docker login -u "$HEROKU_USERNAME" --password-stdin "$HEROKU_REGISTRY"
+# docker build --target production --force-rm -t registry.heroku.com/value-investment/web:latest .
+# docker push registry.heroku.com/value-investment/web:latest
 
-docker build --target production --force-rm -t registry.heroku.com/value-investment/web:latest .
-docker push registry.heroku.com/value-investment/web:latest
+# image_id=$(docker inspect registry.heroku.com/value-investment/web:latest --format={{.Id}})
+# echo Releasing image to Heroku: $image_id
 
-image_id=$(docker inspect registry.heroku.com/value-investment/web:latest --format={{.Id}})
-echo Releasing image to Heroku: $image_id
+# get_msg()
+# {
+# cat <<EOF
+# {
+#   "updates": [
+#     {
+#       "type": "web",
+#       "docker_image": "$image_id"
+#     }
+#   ]
+# }
+# EOF
+# }
 
-get_msg()
-{
-cat <<EOF
-{
-  "updates": [
-    {
-      "type": "web",
-      "docker_image": "$image_id"
-    }
-  ]
-}
-EOF
-}
-
-curl -n -X PATCH https://api.heroku.com/apps/value-investment/formation \
--d "$(get_msg)" \
--H "Content-Type: application/json" \
--H "Accept: application/vnd.heroku+json; version=3.docker-releases" \
--H "Authorization: Bearer $HEROKU_API_KEY"
+# curl -n -X PATCH https://api.heroku.com/apps/value-investment/formation \
+# -d "$(get_msg)" \
+# -H "Content-Type: application/json" \
+# -H "Accept: application/vnd.heroku+json; version=3.docker-releases" \
+# -H "Authorization: Bearer $HEROKU_API_KEY"
