cd8188b98f4b5fb383f2e61dc2184975bc77501b
Fischer-L <Fischer-L@users.noreply.github.com>
2020-01-06 22:51:23 +0800

    Refactor to adopt `onClickDo` (#104)

--- src/client/components/subcomponents/ClickableComponent.js
+++ src/client/components/subcomponents/ClickableComponent.js
@@ -1,38 +1,6 @@
 import { Component } from 'react';
 
 class ClickableComponent extends Component {
-  constructor(props) {
-    super(props);
-
-    this._handlers = [];
-
-    this.onClick = e => {
-      if (this._handlers.length === 0) return;
-
-      if (e.type === 'touchend') this._isTouchHandled = true;
-
-      if (e.type === 'click' && this._isTouchHandled) {
-        e.stopPropagation();
-        return;
-      }
-
-      e.persist();
-      let { target } = e;
-      while (target) {
-        for (const handler of this._handlers) { // eslint-disable-line no-restricted-syntax
-          if (handler(e, target)) {
-            e.preventDefault();
-            e.stopPropagation();
-            return;
-          }
-        }
-        if (this._stopTraversingDOM(e, target)) {
-          target = null;
-        }
-        target = target && target.parentElement;
-      }
-    };
-  }
 
   onClickDo(callback) {
     return e => {
@@ -49,14 +17,6 @@ class ClickableComponent extends Component {
     if (typeof this.props[name] === 'function') {
       this.props[name](payload);
     }
-  }
-
-  _stopTraversingDOM(e, target) { // eslint-disable-line no-unused-vars
-    return false;
-  }
-
-  regisOnClick(...handlers) {
-    this._handlers.push(...handlers.filter(h => typeof h === 'function'));
   }
 }
 
