55322216413184b200d246f2cb75072300666bae
fischer.liu <fischer.liu@linecorp.com>
2020-02-27 17:28:26 +0800

    Add middle eps

--- src/client/components/Table.jsx
+++ src/client/components/Table.jsx
@@ -2,10 +2,9 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Table } from 'semantic-ui-react';
 import { round } from '@/utils/index';
+import show from '@/utils/showDisplay';
 
 import '@/css/Table.scss';
-
-const subTextClass = subText => `table-subText ${subText ? '' : 'app-none'}`;
 
 function renderHeader(headerSubText = {}) {
   const { low, mid, top } = headerSubText;
@@ -13,11 +12,11 @@ function renderHeader(headerSubText = {}) {
     <Table.Header>
       <Table.Row>
         <Table.HeaderCell />
-        <Table.HeaderCell>Low<p className={subTextClass(low)}>({ low })</p></Table.HeaderCell>
+        <Table.HeaderCell>Low<p className="table-subText" style={show(low)}>({ low })</p></Table.HeaderCell>
         <Table.HeaderCell></Table.HeaderCell>
-        <Table.HeaderCell>Mid<p className={subTextClass(mid)}>({ mid })</p></Table.HeaderCell>
+        <Table.HeaderCell>Mid<p className="table-subText" style={show(mid)}>({ mid })</p></Table.HeaderCell>
         <Table.HeaderCell></Table.HeaderCell>
-        <Table.HeaderCell>Top<p className={subTextClass(top)}>({ top })</p></Table.HeaderCell>
+        <Table.HeaderCell>Top<p className="table-subText" style={show(top)}>({ top })</p></Table.HeaderCell>
       </Table.Row>
     </Table.Header>
   );
@@ -26,11 +25,16 @@ function renderHeader(headerSubText = {}) {
 function Row({ title, cells }) {
   const tableCells = cells.map(([ content, subText ], i) => {
     const key = i + content + (subText || '');
-    return <Table.Cell key={key}>{ content || '' }<p className={subTextClass(subText)}>{ subText }</p></Table.Cell>;
+    return (
+      <Table.Cell key={key} className={i % 2 === 1 ? 'table-subText' : ''}>
+        { content || '' }
+        <p className="table-subText" style={show(subText)}>({ subText })</p>
+      </Table.Cell>
+    );
   });
   return (
     <Table.Row>
-      <Table.Cell>{ title[0] }<p className={subTextClass(title[1])}>{ title[1] }</p></Table.Cell>
+      <Table.Cell>{ title[0] }<p className="table-subText" style={show(title[1])}>({ title[1] })</p></Table.Cell>
       { tableCells }
     </Table.Row>
   );
@@ -42,8 +46,8 @@ Row.propTypes = {
 
 class TableByYears extends Component {
   render() {
-    const [ [top5Price, top5Eps], [mid5Price, mid5Eps], [low5Price, low5Eps] ] = this.props.prices5yrs;
-    const [ [top3Price, top3Eps], [mid3Price, mid3Eps], [low3Price, low3Eps] ] = this.props.prices3yrs;
+    const [ [top5yrsPrice, top5yrsFactor], [mid5yrsPrice, mid5yrsFactor], [low5yrsPrice, low5yrsFactor] ] = this.props.prices5yrs;
+    const [ [top3yrsPrice, top3yrsFactor], [mid3yrsPrice, mid3yrsFactor], [low3yrsPrice, low3yrsFactor] ] = this.props.prices3yrs;
     return (
       <Table unstackable selectable color={this.props.color} className="table">
         { renderHeader() }
@@ -51,21 +55,21 @@ class TableByYears extends Component {
           { Row({
             title: [ '5 Years' ],
             cells: [
-              [ low5Price, `(${low5Eps})` ],
-              [ '', round((low5Price + mid5Price) / 2) ],
-              [ mid5Price, `(${mid5Eps})` ],
-              [ '', round((mid5Price + top5Price) / 2) ],
-              [ top5Price, `(${top5Eps})` ],
+              [ low5yrsPrice, low5yrsFactor ],
+              [ round((low5yrsPrice + mid5yrsPrice) / 2), round((low5yrsFactor + mid5yrsFactor) / 2) ],
+              [ mid5yrsPrice, mid5yrsFactor ],
+              [ round((mid5yrsPrice + top5yrsPrice) / 2), round((mid5yrsFactor + top5yrsFactor) / 2) ],
+              [ top5yrsPrice, top5yrsFactor ],
             ],
           }) }
           { Row({
             title: [ '3 Years' ],
             cells: [
-              [ low3Price, `(${low3Eps})` ],
-              [ '', round((low3Price + mid3Price) / 2) ],
-              [ mid3Price, `(${mid3Eps})` ],
-              [ '', round((mid3Price + top3Price) / 2) ],
-              [ top3Price, `(${top3Eps})` ],
+              [ low3yrsPrice, low3yrsFactor ],
+              [ round((low3yrsPrice + mid3yrsPrice) / 2), round((low3yrsFactor + mid3yrsFactor) / 2) ],
+              [ mid3yrsPrice, mid3yrsFactor ],
+              [ round((mid3yrsPrice + top3yrsPrice) / 2), round((mid3yrsFactor + top3yrsFactor) / 2) ],
+              [ top3yrsPrice, top3yrsFactor ],
             ],
           }) }
         </Table.Body>
@@ -87,12 +91,12 @@ class TableByDividends extends Component {
   renderRow(title, data) {
     const [ dividend, topPrice, midPrice, lowPrice ] = data;
     return Row({
-      title: [ title, `(${dividend})` ],
+      title: [ title, dividend ],
       cells: [
         [ lowPrice ],
-        [ '', round((lowPrice + midPrice) / 2) ],
+        [ round((lowPrice + midPrice) / 2) ],
         [ midPrice ],
-        [ '', round((midPrice + topPrice) / 2) ],
+        [ round((midPrice + topPrice) / 2) ],
         [ topPrice ],
       ],
     });
