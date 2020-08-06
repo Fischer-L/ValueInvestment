7d6cb8e929ccaec301b9b472affbe82d5b0b1147
Fischer-L <Fischer-L@users.noreply.github.com>
2020-08-06 12:59:16 +0800

    Update handleError on the server side (#190)

--- src/server/routes/stockdataRoute.js
+++ src/server/routes/stockdataRoute.js
@@ -5,13 +5,13 @@ const axios = require('axios');
 // const cloudscraper = require('../../lib/myCloudscraper');
 
 const CacheProvider = require('../cacheProvider');
-const { env } = require('../../build/config_server');
+const handleError = require('../utils/handleError');
 const GwServer = require('../../lib/stockProvider/GwServer');
 const GooServer = require('../../lib/stockProvider/GooServer');
 
 const stockdataCache = new CacheProvider();
-const gwStockProvider = new GwServer({ env, axios });
-const gooStockProvider = new GooServer({ env, axios });
+const gwStockProvider = new GwServer({ axios });
+const gooStockProvider = new GooServer({ axios });
 
 function shouldInvalidateCache(req) {
   const { noCache } = req.query;
@@ -20,33 +20,38 @@ function shouldInvalidateCache(req) {
 
 function initStockdataRoute(app) {
   app.get('/stockdata/:id', async (req, res) => {
-    const cacheKey = req.path;
+    let data = null;
+    try {
+      const cacheKey = req.path;
 
-    if (shouldInvalidateCache(req)) {
-      stockdataCache.remove(cacheKey);
-    }
-
-    let data = stockdataCache.get(cacheKey);
-    if (!data) {
-      const providers = [
-        [ 'gwStockData', gwStockProvider ],
-        [ 'gooStockData', gooStockProvider ],
-      ];
-      const dataArray = await Promise.all(
-        providers.map(provider => provider[1].get(req.params.id, req.query)),
-      );
-
-      data = { error: '' };
-      providers.forEach(([ key ], i) => {
-        if (dataArray[i].error) {
-          data.error += ' / ' + dataArray[i].error;
-        } else {
-          data[key] = dataArray[i];
-        }
-      });
-      if (!data.error) {
-        stockdataCache.set(cacheKey, data);
+      if (shouldInvalidateCache(req)) {
+        stockdataCache.remove(cacheKey);
       }
+
+      data = stockdataCache.get(cacheKey);
+      if (!data) {
+        const providers = [
+          [ 'gwStockData', gwStockProvider ],
+          [ 'gooStockData', gooStockProvider ],
+        ];
+        const dataArray = await Promise.all(
+          providers.map(provider => provider[1].get(req.params.id, req.query)),
+        );
+
+        data = { error: '' };
+        providers.forEach(([ key ], i) => {
+          if (dataArray[i].error) {
+            data.error += ' / ' + dataArray[i].error;
+          } else {
+            data[key] = dataArray[i];
+          }
+        });
+        if (!data.error) {
+          stockdataCache.set(cacheKey, data);
+        }
+      }
+    } catch (e) {
+      data = handleError(e);
     }
     res.json(data);
   });
