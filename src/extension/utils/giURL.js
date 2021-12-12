4f91a54c566d9c19d15e5119bf01b5e5eb438405
fischer.liu <fischer.liu@linecorp.com>
2021-12-13 01:38:14 +0800

    Fix gi path

--- src/extension/utils/giURL.js
+++ src/extension/utils/giURL.js
@@ -8,7 +8,7 @@ export default function giURL(type) {
   let path = '';
   switch (type) {
     case PATH_TYPE.LIST:
-      path = [ '/', 'Stock', 'Info', '/', 'Stock', 'List.asp' ].join('');
+      path = [ '/t', 'w', '/', 'Stock', 'List.asp' ].join('');
       break;
   }
   return DOMAINS.gi + path;
