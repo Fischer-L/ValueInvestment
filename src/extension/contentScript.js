a5cec28f4f46f82d7af37ff02575d85fcdd58b08
Fischer-L <fischer.json@gmail.com>
2023-04-11 22:36:40 +0800

    Accept multiple stocks at once

--- src/extension/contentScript.js
+++ src/extension/contentScript.js
@@ -47,95 +47,100 @@ const hotKeysManager = {
     }
   },
 };
+
 hotKeysManager.hotKeys.push({
   targetKeys: [ 'meta', '\'', 'enter' ],
+
   isTargetPage() {
     return true;
   },
+
   exec() {
     const input = (window.prompt('Enter id') || '').trim();
     if (!input) {
       return;
     }
 
-    const cmds = [];
-    let [ instructions, stockId ] = input.split(' ');
-
-    if (!stockId && instructions) {
-      stockId = instructions;
-      instructions = 't';
-    }
+    let [ instructions, ...stockIds ] = input.split(' ');
 
     if (instructions === '`') {
+      // case: '` 5678' or '` 5678 8765'
       instructions = '?tif';
+    } else if (!Number.isNaN(parseInt(instructions, 10))) {
+      // case: '5678' or '5678 8765'
+      stockIds = input.split(' ');
+      instructions = 't';
     }
+    stockIds.forEach(stockId => this._exec(stockId, instructions));
+  },
 
-    if (stockId && instructions) {
-      for (let i = instructions.length - 1; i >= 0; i--) {
-        switch (instructions[i]) {
-          case '?':
-            cmds.push({
-              cmd: 'CMD_WHAT_IS_STOCK',
-              params: {
-                stockId,
-              },
-            });
-            break;
+  _exec(stockId, instructions) {
+    const cmds = [];
 
-          case 'h':
-            cmds.push({
-              cmd: 'CMD_OPEN_URL',
-              params: {
-                url: getURL(SITE.big_holder, { stockId }),
-              },
-            });
-            break;
+    for (let i = instructions.length - 1; i >= 0; i--) {
+      switch (instructions[i]) {
+        case '?':
+          cmds.push({
+            cmd: 'CMD_WHAT_IS_STOCK',
+            params: {
+              stockId,
+            },
+          });
+          break;
 
-          case 'f':
-            cmds.push({
-              cmd: 'CMD_OPEN_URL',
-              params: {
-                url: getURL(SITE.tw_finance, null, { stockId }),
-              },
-            });
-            break;
+        case 'h':
+          cmds.push({
+            cmd: 'CMD_OPEN_URL',
+            params: {
+              url: getURL(SITE.big_holder, { stockId }),
+            },
+          });
+          break;
 
-          case 'i':
-            cmds.push({
-              cmd: 'CMD_OPEN_URL',
-              params: {
-                url: getURL(SITE.info, null, { stockId }),
-              },
-            });
-            break;
+        case 'f':
+          cmds.push({
+            cmd: 'CMD_OPEN_URL',
+            params: {
+              url: getURL(SITE.tw_finance, null, { stockId }),
+            },
+          });
+          break;
 
-          case 'm':
-            cmds.push({
-              cmd: 'CMD_OPEN_URL',
-              params: {
-                url: getURL(SITE.margin, { stockId }),
-              },
-            });
-            break;
+        case 'i':
+          cmds.push({
+            cmd: 'CMD_OPEN_URL',
+            params: {
+              url: getURL(SITE.info, null, { stockId }),
+            },
+          });
+          break;
 
-          case 'd':
-            cmds.push({
-              cmd: 'CMD_OPEN_URL',
-              params: {
-                url: getURL(SITE.forum, null, { stockId }),
-              },
-            });
-            break;
+        case 'm':
+          cmds.push({
+            cmd: 'CMD_OPEN_URL',
+            params: {
+              url: getURL(SITE.margin, { stockId }),
+            },
+          });
+          break;
 
-          case 't': default:
-            cmds.push({
-              cmd: 'CMD_OPEN_URL',
-              params: {
-                url: getURL(SITE.technical, null, { stockId }),
-              },
-            });
-            break;
-        }
+        case 'd':
+          cmds.push({
+            cmd: 'CMD_OPEN_URL',
+            params: {
+              url: getURL(SITE.forum, null, { stockId }),
+            },
+          });
+          break;
+
+        case 't': default:
+          cmds.push({
+            cmd: 'CMD_OPEN_URL',
+            params: {
+              url: getURL(SITE.technical, null, { stockId }),
+            },
+          });
+          break;
       }
     }
 
