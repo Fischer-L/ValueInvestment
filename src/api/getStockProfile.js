4191052d6931bb12ea0779574fa1984e19807f07
Fischer-L <fischer.json@gmail.com>
2024-04-04 17:57:06 +0800

    Migrate to Chrome Extension V3

--- src/api/getStockProfile.js
+++ src/api/getStockProfile.js
@@ -1,12 +1,12 @@
-import axios from 'axios';
 import DOMAINS from '~/utils/domains';
 
 export default async function getStockProfile(stockId) {
   const trailPath = [ 'FC', 'NT0', '00017', '?sym', 'bol_id=', stockId ].join('');
   const url = [ DOMAINS.fg, 'api', 'v2', 'data', 'contents', trailPath ].join('/');
-  const result = await axios.get(url);
+  const response = await fetch(url);
+  const json = await response.json();
   return {
     id: stockId,
-    name: result.data.data.content.rawContent.name,
+    name: json.data.content.rawContent.name,
   };
 }
